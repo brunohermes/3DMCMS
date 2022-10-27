09a8e11dfaae532b865a9b686b936b7cca9f0227
Bruno Hermes <48295386+brunohermes@users.noreply.github.com>
2022-10-27 16:31:33 -0300

    Database Setup

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php 
+    //Sets up the database connection
+    class databaseSetup{
+        private $dbhost;
+        private $dbuser;
+        private $dbpwd;
+        private $dbname;
+
+        protected function connect(){
+            $this->dbhost = "localhost";
+            $this->dbuser = "root";
+            $this->dbpwd = "";
+            $this->dbname = "3dm";
+
+            $conn = new mysqli();
+        }
+    }
+
+?>
+
