ddfbe4d64963f875df4b3933940644850dd804c7
Bruno Hermes <48295386+brunohermes@users.noreply.github.com>
2022-10-29 20:41:46 -0300

    Design
    major changes.

--- index.php
+++ index.php
@@ -19,16 +19,27 @@
     <embed src="resources/designcomp.svg"/>
           <form class="loginform" action="includes/login.inc.php" method="post">
             <div class="designform">
-            </div>
+            <h1>Sign in</h1>
+        </div>
             <input class="logininput" type="text" name="uid" placeholder="|  Username"><br>
             <input class="pwdinput" type="password" name="pwd" placeholder="|  Password"><br>
             <button class="login-button" type="submit" name="submit">Continue</button>
+            
             <div>
             <span class="loginw"><hr class="hr1"></hr>or Sign in with<hr class="hr2"></hr></span>
             <center><span align="center" class="social-icons"><a href="#"><img class="social" src="resources/facebook.png"></a><a href="#"><img class="social" src="resources/google.png"></a><a href="#"><img class="social" src="resources/twitter.png"></a></span></center>
-            </div>
+             
+        </div>
            </form>
     </div>
 
 </body>
+
+<footer>
+    <div class="footer">
+        <hr class="footerhr"></hr>
+        <a class="footerlink" href="#">Link #1</a> <a class="footerlink" href="#">Link #2</a> <a class="footerlink" href="#">Link #3</a> <a class="footerlink" href="#">Link #5</a>
+    </div>
+
+</footer>
 </html>
